29e6ea51fa6a3f1905679fcaf20a0e30b9a38efa
Kutafilec <63808575+Kutafilec@users.noreply.github.com>
2022-01-04 19:24:03 +0100

    Sprawdzanie wyniku dla delta>0

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -25,9 +25,35 @@ namespace RownanieKwadratowe1.Testy
             double wynikDelty=o1.delta;
 
 
-            //assert regerhgerhr
+            //assert
 
             Assert.Equal(wynikDelty, wynik);
+        }
+
+
+        [Fact]
+        public void liczRownanie_SprawdzanieObliczeñDlaPierwiastkow_dlaDeltyDodatniej()
+        {
+            //arange
+            RownanieKwadratowe o2 = new RownanieKwadratowe();
+            o2.a = -2;
+            o2.b = 1;
+            o2.c = 3;
+
+            //act
+            o2.liczRownanie();
+            double wynik_x_1 = o2.x1;
+            double wynik_x_2 = o2.x2;
+            double wynik_prawidlowy_x1 = 1.5;
+           // double wynik_prawidlowy_x2 = 1.5;
+
+            //assert
+            Assert.Equal(wynik_prawidlowy_x1, wynik_x_1 );
+            //Assert.Equal(wynik_prawidlowy_x2, wynik_x_2);
+
+
+            
+
         }
     }
 }
