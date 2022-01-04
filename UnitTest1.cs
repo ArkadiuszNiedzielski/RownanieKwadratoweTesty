efbc0bbdfbb902642ca099aceaba6d38d33f6c84
Kutafilec <63808575+Kutafilec@users.noreply.github.com>
2022-01-04 15:21:24 +0100

    ...

---
+++ UnitTest1.cs
@@ -0,0 +1,35 @@
+using System;
+using Xunit;
+
+namespace RownanieKwadratowe1.Testy
+{
+    public class RownanieKwadratoweTesty
+    {
+        [Theory]
+        [InlineData(-2,1,3,25)]
+        [InlineData(-3, 0, 4, 48)]
+        [InlineData(2, 1, 1, -7)]
+        [InlineData(2, 4, 2, 0)]
+
+        public void liczRownanie_SprawdzanieObliczeñDlaDelty_i_wynikow(double a, double b, double c, double wynik)
+        {
+            //arrange
+            RownanieKwadratowe o1 = new RownanieKwadratowe();
+            o1.a = a;
+            o1.b = b;
+            o1.c = c;
+
+            //act
+
+            o1.liczRownanie();
+            double wynikDelty=o1.delta;
+
+
+            //assert regerhgerhr
+
+            Assert.Equal(wynikDelty, wynik);
+
+
+        }
+    }
+}
