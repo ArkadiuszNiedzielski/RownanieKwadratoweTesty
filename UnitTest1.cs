ffd4ede85772b1710458f4e9e63e4e73bba693ae
Kutafilec <63808575+Kutafilec@users.noreply.github.com>
2022-01-04 19:32:13 +0100

    delta>0 zmiana [Fact] na [Theory]
    działa

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -31,28 +31,24 @@ namespace RownanieKwadratowe1.Testy
         }
 
 
-        [Fact]
-        public void liczRownanie_SprawdzanieObliczeñDlaPierwiastkow_dlaDeltyDodatniej()
+        [Theory]
+        [InlineData(-2, 1, 3, 25,1.5,-1)]
+        /*[InlineData(-3, 0, 4, 48)]
+        [InlineData(2, 1, 1, -7)]
+        [InlineData(2, 4, 2, 0)]*/
+        public void liczRownanie_SprawdzanieObliczeñDlaPierwiastkow_dlaDeltyDodatniej(double a, double b, double c, double delta, double wynik1, double wynik2)
         {
             //arange
             RownanieKwadratowe o2 = new RownanieKwadratowe();
-            o2.a = -2;
-            o2.b = 1;
-            o2.c = 3;
 
             //act
             o2.liczRownanie();
-            double wynik_x_1 = o2.x1;
-            double wynik_x_2 = o2.x2;
-            double wynik_prawidlowy_x1 = 1.5;
-            double wynik_prawidlowy_x2 = -1;
+            wynik1 = o2.x1;
+            wynik2 = o2.x2;
 
             //assert
-            Assert.Equal(wynik_prawidlowy_x1, wynik_x_1);
-            Assert.Equal(wynik_prawidlowy_x2, wynik_x_2);
-
-
-            
+            Assert.Equal(wynik1, o2.x1);
+            Assert.Equal(wynik2, o2.x2);
 
         }
     }
